e1998cc679aa5568a75405eceb61c33e0b6fe982
habbiner <habbiner.andrade@gmail.com>
2025-05-31 20:42:37 -0300

    correção rota tela inicial web

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import TheDashBoard from '@/views/TheDashBoard.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/dashboard',
+      path: '/',
       name: 'Dashboard',
       component: TheDashBoard,
       meta: {
         title: 'AgroTech | Dash'
       }
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
     },
   ],
 })
